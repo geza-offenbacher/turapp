8c409f5a05b0ee5825b0a697919adf7c1848de78
Géza Offenbächer <43416045+geza-offenbacher@users.noreply.github.com>
2020-05-15 08:47:52 +0200

    Deployra előkészítve

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
+
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -29,6 +31,16 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 app.use("/api/hikes", hikes);
+
+// Deploy
+if (process.env.NODE_ENV === 'production') {
+    // static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
